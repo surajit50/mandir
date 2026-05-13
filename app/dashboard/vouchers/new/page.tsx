6f2d461a9d1d81cfc6fde31dc6ded9ab2d9aa65b
Surajit Shil <38415007+surajit50@users.noreply.github.com>
2026-05-13 22:22:08 +0530

    Update page.tsx

--- app/dashboard/vouchers/new/page.tsx
+++ app/dashboard/vouchers/new/page.tsx
@@ -2,7 +2,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation";
 import { PaymentVoucherForm } from "@/components/forms/voucher-form/payment-voucher-form";
-
+  
 export default async function NewVoucherPage() {
   const session = await getServerSession(authOptions);
   
