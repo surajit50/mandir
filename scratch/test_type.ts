28138e4c6abaa2e1f3ef7248578e9aed12815510
Surajit Shil <no3dhalparagp@gmail.com>
2026-05-14 13:42:50 +0530

    'fafafa'

--- scratch/test_type.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-type TransactionClient = Parameters<Parameters<typeof prisma.$transaction>[0]>[0];
-
-async function test() {
-  await prisma.$transaction(async (tx: TransactionClient) => {
-    // test
-  });
-}
